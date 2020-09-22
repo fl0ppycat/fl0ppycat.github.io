d3191ebedd2804deaeee3a86688b9fa6b37a3fc3
fl0ppycat <floppycatcher@gmail.com>
2020-09-22 13:57:54 +0300

    js textarea input string generator homework - one word string input fixed

--- 2020-09-19_js_textarea_homework/js/main.js
+++ 2020-09-19_js_textarea_homework/js/main.js
@@ -6,6 +6,7 @@ let buttonElement = document.querySelector('.goButton');
 
 let textByWordsArray = [];
 
+
 function randomInteger(min, max) {
     let rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
@@ -15,16 +16,23 @@ function randomInteger(min, max) {
 
 goButtonFunction = () => {
 
+    textByWordsArray = [];
+    mainElement.innerHTML = "";
+
     let nextWordStartSymbol = 0;
+
+    let nextWord = textareaElement.value;
+
+    textareaElement.value = textareaElement.value + " "
 
     for (let i = 0; i < textareaElement.value.length; i++){  
         if (textareaElement.value[i] == " "){
-            let nextWord = "";
+            nextWord = "";
             for (let j = nextWordStartSymbol; j < i; j++){
                 nextWord = nextWord + String(textareaElement.value[j]);
             }
             nextWordStartSymbol = i+1;
-            mainElement.innerHTML = mainElement.innerHTML + `<input id="id${i}" class="input">`
+            mainElement.innerHTML = mainElement.innerHTML + `<input class="input">`
             // inputElementsArray.push(document.getElementById(`id${i}`));
             // document.getElementById(`id${i}`).value = nextWord;
             textByWordsArray.push(nextWord);
@@ -35,6 +43,12 @@ goButtonFunction = () => {
     let theLongesWord = textByWordsArray[0];
 
     for (let i = 0; i < inputElementsArray.length; i++){
+        // if (textByWordsArray[i] == ""){
+        //     inputElementsArray.splice(i,1);
+        //     textByWordsArray.splice(i,1);
+        //     i++;
+        // }
+        
         inputElementsArray[i].value = textByWordsArray[i];
         if (theLongesWord.length < textByWordsArray[i].length){
             theLongesWord = textByWordsArray[i];
@@ -46,19 +60,21 @@ goButtonFunction = () => {
     document.querySelector('.randomButton').style.visibility = "visible";
 }
 
+buttonElement.addEventListener('click',goButtonFunction);
+
 let randomButtonElement = document.querySelector('.randomButton');
 
 randomButtonFunction = () => {
 
     thirtySymbolsString = "";
     while(thirtySymbolsString.length != 30){
-        if((thirtySymbolsString.length > 30) || (thirtySymbolsString.length == 0)){thirtySymbolsString = `${textByWordsArray[randomInteger(1,textByWordsArray.length-1)]}`}
+        if((thirtySymbolsString.length > 30) || (thirtySymbolsString.length == 0)){
+            thirtySymbolsString = `${textByWordsArray[randomInteger(1,textByWordsArray.length-1)]}`
+        }
         thirtySymbolsString = `${thirtySymbolsString} ${textByWordsArray[randomInteger(1,textByWordsArray.length-1)]}`;
     }        
 
     console.log(`thirty Symbols String = ${thirtySymbolsString}`);
 }
-
-buttonElement.addEventListener('click',goButtonFunction);
 
 randomButtonElement.addEventListener('click',randomButtonFunction);
