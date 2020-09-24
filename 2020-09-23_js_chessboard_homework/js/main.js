f33e468315835bd3449e264c275cfe37a4a4b3fe
fl0ppycat <floppycatcher@gmail.com>
2020-09-24 11:54:10 +0300

    js chessboard homework fixed

--- 2020-09-23_js_chessboard_homework/js/main.js
+++ 2020-09-23_js_chessboard_homework/js/main.js
@@ -5,59 +5,87 @@ let CHARS = "ABCDEFGH"
 
 let str = ``;
 
-let cellsArray = [];
+let cellsElementsArray = [];
 
+figureById = (id) => {
+    switch (id) {
+        case "A7":
+        case "B7":
+        case "C7":
+        case "D7": 
+        case "E7":
+        case "F7":
+        case "G7":
+        case "H7":   
+          return("&#9823;");
+        
+        case "A8":
+        case "H8":
+            return("&#9820;");
+        
+        case "B8":
+        case "G8":
+            return("&#9822;");
+        
+        case "C8":
+        case "F8":
+            return("&#9821;");
+        
+        case "D8":
+            return("&#9819;");
+
+        case "E8":
+            return("&#9818;");
+ 
+        default:
+            return(``);
+      }
+}
 
 for (let i = 8; i > 0; i--){
-    cellsArray[i] = [];
     for (let j = 0; j < 8; j++){
-        str = `<div id="${CHARS[j]}${i}" class="cell"`;
+        str = `<div id="${CHARS[j]}${i}" class="cell row${i}"`;
 
         if( ((i + j) % 2) == 0){
-            str = `${str} style="background-color: grey;">${CHARS[j]}${i}</div>`;} else {
-                str = `${str}>${CHARS[j]}${i}</div>`;
+            str = `${str} style="background-color: grey;"></div>`;} else {
+                str = `${str}></div>`;
         }
 
-        chessboardDiv.innerHTML = `${chessboardDiv.innerHTML} ${str}`;
-    }
-}
-
-
-for (let i = 8; i > 0; i--){
-    for (let j = 0; j < 8; j++){ 
-        cellsArray[i][j] = document.getElementById(`${CHARS[j]}${i}`);
-
-        cellsArray[i][j].innerHTML = "";
-
-        if(i == 7){cellsArray[i][j].innerHTML = "&#9823;";}
-        if(i == 8){
-            if((j == 0) || (j == 7)){cellsArray[i][j].innerHTML = "&#9820;";}
-            if((j == 1) || (j == 6)){cellsArray[i][j].innerHTML = "&#9822;";}
-            if((j == 2) || (j == 5)){cellsArray[i][j].innerHTML = "&#9821;";}
-
-            if(j == 3){cellsArray[i][j].innerHTML = "&#9819;";}
-            if(j == 4){cellsArray[i][j].innerHTML = "&#9818;";}  
-        }
+        chessboardDiv.innerHTML = `${chessboardDiv.innerHTML} ${str}`; 
         
-        if(i == 2){cellsArray[i][j].innerHTML = "&#9817;";}
-        if(i == 1){
-            if((j == 0) || (j == 7)){cellsArray[i][j].innerHTML = "&#9814;";}
-            if((j == 1) || (j == 6)){cellsArray[i][j].innerHTML = "&#9816;";}
-            if((j == 2) || (j == 5)){cellsArray[i][j].innerHTML = "&#9815;";}
-
-            if(j == 3){cellsArray[i][j].innerHTML = "&#9812;";}
-            if(j == 4){cellsArray[i][j].innerHTML = "&#9813;";}  
-        }  
+        document.getElementById(`${CHARS[j]}${i}`).innerHTML = figureById(`${CHARS[j]}${i}`);
     }
+    cellsElementsArray[8-i] = document.querySelectorAll(`.row${i}`);
 }
 
-
-// console.table(cellsArray);
-
+console.table(cellsElementsArray);
 
 
-// console.log(`${cellsArray[2][5].innerHTML}`);
 
-// cellsArray[2][5].innerHTML = `AAAAAAAA`;
+// for (let i = 8; i > 0; i--){
+//     for (let j = 0; j < 8; j++){ 
+//         cellsArray[i][j] = document.getElementById(`${CHARS[j]}${i}`);
 
-// console.log(`${cellsArray[2][5].innerHTML}`);
+//         cellsArray[i][j].innerHTML = "";
+
+//         if(i == 7){cellsArray[i][j].innerHTML = "&#9823;";}
+//         if(i == 8){
+//             if((j == 0) || (j == 7)){cellsArray[i][j].innerHTML = "&#9820;";}
+//             if((j == 1) || (j == 6)){cellsArray[i][j].innerHTML = "&#9822;";}
+//             if((j == 2) || (j == 5)){cellsArray[i][j].innerHTML = "&#9821;";}
+
+//             if(j == 3){cellsArray[i][j].innerHTML = "&#9819;";}
+//             if(j == 4){cellsArray[i][j].innerHTML = "&#9818;";}  
+//         }
+        
+//         if(i == 2){cellsArray[i][j].innerHTML = "&#9817;";}
+//         if(i == 1){
+//             if((j == 0) || (j == 7)){cellsArray[i][j].innerHTML = "&#9814;";}
+//             if((j == 1) || (j == 6)){cellsArray[i][j].innerHTML = "&#9816;";}
+//             if((j == 2) || (j == 5)){cellsArray[i][j].innerHTML = "&#9815;";}
+
+//             if(j == 3){cellsArray[i][j].innerHTML = "&#9812;";}
+//             if(j == 4){cellsArray[i][j].innerHTML = "&#9813;";}  
+//         }  
+//     }
+// }
