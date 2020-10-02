2e188a6bf0d5d687d8188835ead4126d92c55b5e
fl0ppycat <floppycatcher@gmail.com>
2020-10-02 16:59:56 +0300

    js train animation homework fixed

--- 2020-09-30_js_train-animation/js/main.js
+++ 2020-09-30_js_train-animation/js/main.js
@@ -1,17 +1,48 @@
 const bodyElement = document.getElementsByTagName("body");
 
+let divControls = document.createElement("div");
+divControls.classList.add('controls');
+bodyElement[0].appendChild(divControls);
+
 let divTrainElement = document.createElement("div");
 divTrainElement.classList.add('train');
-divTrainElement.innerHTML = "&#128642;"
+divTrainElement.innerHTML = `<img src="../images/train4.png"></img>`
 
 let divRailElement = document.createElement("div");
 divRailElement.classList.add('rail');
 
-let divLightElement = document.createElement("div");
-
 bodyElement[0].appendChild(divRailElement);
 bodyElement[0].appendChild(divTrainElement);
+
+let divLightElement = document.createElement("div");
+
 divTrainElement.appendChild(divLightElement);
+
+
+
+
+
+let buttonRightElment = document.createElement("button");
+buttonRightElment.innerHTML = "move Right >";
+let buttonLeftElment = document.createElement("button");
+buttonLeftElment.innerHTML = "< move Left";
+let buttonLightElment = document.createElement("button");
+buttonLightElment.innerHTML = "toogle Light";
+
+divControls.appendChild(buttonLeftElment);
+divControls.appendChild(buttonLightElment);
+divControls.appendChild(buttonRightElment);
+
+buttonRightElment.addEventListener('mousedown', eventListener => {
+  moveFunction(divTrainElement,true);
+});
+buttonLeftElment.addEventListener('mousedown', eventListener => {
+  moveFunction(divTrainElement,false);
+});
+buttonLightElment.addEventListener('click', eventListener => {
+  divLightElement.classList.toggle("light");
+});
+
 
 let nextPosition = "0px";
 let currentDirection = false;
@@ -43,6 +74,8 @@ let eventListener = document.addEventListener('keydown', (event) => {
       console.log(`event.code: ${event.code}, event.key: ${event.key}`);
   }
 })
+
+
 
 
 
