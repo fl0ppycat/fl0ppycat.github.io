65e7b8eec64bc803875837456d0c8fcb542ed24a
fl0ppycat <floppycatcher@gmail.com>
2020-10-11 17:59:24 +0300

    js snake game practice and homework finished

--- 2020-10-09_js/js/main.js
+++ 2020-10-09_js/js/main.js
@@ -11,7 +11,7 @@ arrayCreator = (n) => {
     }
 
     return array;
-};
+}
 
 let someArray = arrayCreator(5);
 // console.table(someArray);
@@ -27,11 +27,12 @@ arrayEreaser = (array) => {
     }
 
     return array;
-};
+}
 
 arrayEreaser(someArray);
 // console.table(someArray);
 
+////////////////////////////////////////////////////////
 // 3) напиши функцию подсчета ненулевых(непустых) клеток во всём массиве
 
 notNullCounter = (array) => {
@@ -41,7 +42,7 @@ notNullCounter = (array) => {
         for (let j = 0; j < array.length; j++){
             if(array[i][j] != null){
                 counter++;
-            };
+            }
         }
     }
 
@@ -50,6 +51,7 @@ notNullCounter = (array) => {
 
 console.log(`Found ${notNullCounter(someArray)} not null elements`);
 
+////////////////////////////////////////////////////////
 // 4) отобрази игровое поле(клетки массива) и строку статуса, в которой будет написано количество ненулевых клеток.
 
 let bodyEl = document.getElementsByTagName("body");
@@ -57,98 +59,159 @@ let bodyEl = document.getElementsByTagName("body");
 let mainDivEl = document.querySelector('.main');
 
 arrayPrinter = (array) => {
-    let counter = 0;
-
     mainDivEl.innerHTML = "";
 
     for (let i = 0; i < array.length; i++){
         for (let j = 0; j < array.length; j++){
             let classString = `class="cell"`;
             if(array[i][j] != null){
-                counter++
                 if(array[i][j] === "1"){ classString = `class="cell one"` }
                 if(array[i][j] === "2"){ classString = `class="cell two"` }
             };
             mainDivEl.innerHTML = `${mainDivEl.innerHTML}<div ${classString}></div>`;
         }
     }
-
     mainDivEl.innerHTML = `${mainDivEl.innerHTML}<div class="counter">Found ${notNullCounter(array)} not null cells</div>`; 
 }
 
 // arrayPrinter(someArray);
 
-// 5) нарисуй 4 кнопки-стрелки: "вверх", "вниз", "влево", "вправо"
 
-let buttonLeftEl = document.createElement("button");
-buttonLeftEl.innerHTML = `&#8592;`;
-bodyEl[0].appendChild(buttonLeftEl);
-let buttonUpEl = document.createElement("button");
-buttonUpEl.innerHTML = `&#8593;`;
-bodyEl[0].appendChild(buttonUpEl);
-let buttonDownEl = document.createElement("button");
-buttonDownEl.innerHTML = `&#8595;`;
-bodyEl[0].appendChild(buttonDownEl);
-let buttonRightEl = document.createElement("button");
-buttonRightEl.innerHTML = `&#8594;`;
-bodyEl[0].appendChild(buttonRightEl);
 
+moveDifs = [];
+moveDifs[0] = new Array(0,-1);
+moveDifs[1] = new Array(-1,0);
+moveDifs[2] = new Array(1,0);
+moveDifs[3] = new Array(0,1);
 
 ////////////////////////////////////////////////////////
-// 6. сделай управление мышкой и клавиатурой, там где находится курсор, закрашиваем клетку синим цветом. Первоначальное положение в левом верхнем углу. В массиве, в соответствующую ячейку писать "1".
+// 5) нарисуй 4 кнопки-стрелки: "вверх", "вниз", "влево", "вправо"
+
+let buttonsElsArray = [];
+
+for (let i = 0; i < moveDifs.length; i++){
+    buttonsElsArray[i] = document.createElement("button");
+    bodyEl[0].appendChild(buttonsElsArray[i]);
+}
+
+buttonsElsArray[0].innerHTML = `&#8592;`;
+buttonsElsArray[1].innerHTML = `&#8593;`;
+buttonsElsArray[2].innerHTML = `&#8595;`;
+buttonsElsArray[3].innerHTML = `&#8594;`;
+
+
+
 
 let x = 0;
 let y = 0;
 
 someArray[x][y] = "1";
 
+////////////////////////////////////////////////////////
+//   8) сделай отключение соответствующих кнопок управления, если курсор с краю поля или наступает на свой след.
+
+checkDirection = (difX, difY) => {  
+    if  ( ( ((y+difY) < 0) || ((x+difX) < 0) ) || ( ((y+difY) > 4) || ((x+difX) > 4) ) ) {
+        return(false)
+    }
+    else {
+        if( (someArray[x+difX][y+difY]) != null) {
+            return(false)
+            }
+            else {
+                return(true)
+            }
+    }
+} 
+
+checkAllDirections = () => { 
+    let blockedDirections = buttonsElsArray.reduce((sum,element,i) => {
+        element = element.disabled = !checkDirection(moveDifs[i][0],moveDifs[i][1]);
+        return sum + element;
+      }, 0); 
+
+    return(blockedDirections);  
+}
+
+
+checkAllDirections();
+
 arrayPrinter(someArray);
 
-moveFunction = (yMove,xMove) => {
+
+gameOver = () => {
+    mainDivEl.innerHTML = `${mainDivEl.innerHTML} <div class="fail">GAME OVER</div>`;
+}
+
+oneMoreTimeQuestion = () => {
+    let gameOverAlert = confirm("Bad news: you failed. Good news: you can try one more time.");
+    if(gameOverAlert){ location.reload(); }
+}
+
+startGameOverTimer = (time) => {
+    tenSecondsTimeout = setTimeout(() => {
+        gameOver();
+    }, time);
+}
+
+startGameOverTimer(10000);
+
+////////////////////////////////////////////////////////
+// 6. сделай управление мышкой и клавиатурой, там где находится курсор, закрашиваем клетку синим цветом. Первоначальное положение в левом верхнем углу. В массиве, в соответствующую ячейку писать "1".
+
+moveFunction = (xMove,yMove) => {
+    clearTimeout(tenSecondsTimeout);
+
     someArray[x][y] = "2";
     x = x + xMove;
     y = y + yMove;
     someArray[x][y] = "1";
 
     arrayPrinter(someArray);
-    // console.table(someArray);
+    
+    if(checkAllDirections() > 3){
+        gameOver();
+        // oneMoreTimeQuestion();
+    }
+
+    startGameOverTimer(10000);
 }
 
 
-buttonLeftEl.addEventListener('click', eventListener => {
-    moveFunction(-1,0);
-});
-buttonUpEl.addEventListener('click', eventListener => {
-    moveFunction(0,-1);
-});
-buttonDownEl.addEventListener('click', eventListener => {
-    moveFunction(0,1);
-});
-buttonRightEl.addEventListener('click', eventListener => {
-    moveFunction(1,0);
-});
+buttonsElsArray.forEach((element, i) => {
+    element.addEventListener('click', eventListener => {
+        moveFunction(moveDifs[i][0],moveDifs[i][1]);
+    });
+})
 
 
 let eventListener = document.addEventListener('keydown', (event) => {
     switch(event.code){
-      case "ArrowRight":{ 
-        moveFunction(1,0);
-        break;
-      }
       case "ArrowLeft":{
-        moveFunction(-1,0);
+        if(checkDirection(0,-1)){    
+            moveFunction(0,-1);
+        }
         break;
       }
       case "ArrowUp":{
-        moveFunction(0,-1);
+        if(checkDirection(-1,0)){    
+            moveFunction(-1,0);
+        }
         break;
       }
       case "ArrowDown":{
-        moveFunction(0,1);
+        if(checkDirection(1,0)){    
+            moveFunction(1,0);
+        }
+        break;
+      }
+      case "ArrowRight":{ 
+        if(checkDirection(0,1)){    
+            moveFunction(0,1);
+        }
         break;
       }
       default:
-        console.log(`Something goes wrong in keydown event listener. event.code: ${event.code}, event.key: ${event.key}`);
+        console.log(`You can't use this key for control. event.code: ${event.code}, event.key: ${event.key}`);
     }
-  })
-
+});
