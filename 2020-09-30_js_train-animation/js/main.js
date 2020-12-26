9f88e9c932fa7b1a1d4632998c95757eb6ca3381
fl0ppycat <floppycatcher@gmail.com>
2020-12-26 12:57:10 +0200

    js train animation homework - pause state added

--- 2020-09-30_js_train-animation/js/main.js
+++ 2020-09-30_js_train-animation/js/main.js
@@ -6,7 +6,9 @@ bodyElement[0].appendChild(divControls);
 
 let divTrainElement = document.createElement("div");
 divTrainElement.classList.add('train');
-divTrainElement.classList.add('moveLeft');
+divTrainElement.classList.add('move');
+divTrainElement.classList.add('flip');
+
 divTrainElement.innerHTML = `<img src="./images/train4.png"></img>`
 
 let divRailElement = document.createElement("div");
@@ -18,10 +20,6 @@ bodyElement[0].appendChild(divTrainElement);
 let divLightElement = document.createElement("div");
 
 divTrainElement.appendChild(divLightElement);
-
-
-
-
 
 let buttonRightElment = document.createElement("button");
 buttonRightElment.innerHTML = "move Right >";
@@ -35,35 +33,49 @@ divControls.appendChild(buttonLightElment);
 divControls.appendChild(buttonRightElment);
 
 buttonRightElment.addEventListener('mousedown', eventListener => {
-  moveFunction(divTrainElement,true);
+  moveFunction(true);
 });
 buttonLeftElment.addEventListener('mousedown', eventListener => {
-  moveFunction(divTrainElement,false);
+  moveFunction(false);
 });
+
 buttonLightElment.addEventListener('click', eventListener => {
   divLightElement.classList.toggle("light");
 });
 
 
-let currentDirection = false;
+let paused = true;
+let currentDirection = true;
 
-moveFunction = (element,nextDirection) => {
-  if (nextDirection != currentDirection){
-    divTrainElement.classList.toggle("moveRight");
-    divTrainElement.classList.toggle("moveLeft");
-    divTrainElement.classList.toggle("flip");
-    currentDirection = nextDirection;
-  }      
+moveFunction = (nextDirection) => {
+  //movement direction. true - right, false - left
+  if(nextDirection) {
+        divTrainElement.style.left = "70vw";
+    } else { divTrainElement.style.left = "5vw"; }
+
+  //"pause" state, if same direction pressed twice
+    if ((!paused) && (nextDirection === currentDirection)){
+        divTrainElement.style.left = divTrainElement.offsetLeft + "px";
+        paused = true;      
+    } else {paused = false}
+
+  //180 degree image flip based on movement direction
+    if (nextDirection != currentDirection){
+      divTrainElement.classList.toggle("flip");
+      currentDirection = nextDirection;
+      paused = false;
+    }
 }
 
+//same for keyboard actions
 let eventListener = document.addEventListener('keydown', (event) => {
   switch(event.code){
     case "ArrowRight":{ 
-      moveFunction(divTrainElement,true);
+      moveFunction(true);
       break;
     }
     case "ArrowLeft":{
-      moveFunction(divTrainElement,false);
+      moveFunction(false);
       break;
     }
     case "KeyF":{
