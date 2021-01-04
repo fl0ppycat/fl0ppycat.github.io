383e3dfd197271db28cbf57129ed86bd87c4a17f
fl0ppycat <floppycatcher@gmail.com>
2021-01-04 19:53:36 +0200

    js traffic lights practice fixed

--- 2021-01-04_js/js/main.js
+++ 2021-01-04_js/js/main.js
@@ -5,6 +5,11 @@ buttonsDivEls = Array.from(document.querySelectorAll(".button"));
 buttonsDivEls.forEach(function(element, i) {
     element.addEventListener('click', eventListener => {
         console.log(`clicked ${eventListener.target.id} button`);
-        lightsDivEls[i].classList.toggle(`${eventListener.target.id}`);
+
+        lightsDivEls.forEach(function(element, i) {
+            element.classList.remove(`${buttonsDivEls[i].id}`);
+        });
+
+        lightsDivEls[i].classList.add(`${eventListener.target.id}`);
     });
 });
